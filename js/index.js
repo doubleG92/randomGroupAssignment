400c8b8c6b5e72a355ab3585c490894d30637f58
doubleG92 <1992gagikgalstyan@gmail.com>
2023-05-26 03:39:20 +0400

    modifed JS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-const gridBox = document.getElementById('grid-box')
-const btn = document.getElementById('shuffleButton')
-const wheelBlock = document.querySelector('.wheel-and-hamster')
+const gridBox = document.getElementById('grid-box');
+const btn = document.getElementById('shuffleButton');
+const wheelBlock = document.querySelector('.wheel-and-hamster');
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const box3 = document.getElementById('box3');
+const box4 = document.getElementById('box4');
+const box5 = document.getElementById('box5');
+const box6 = document.getElementById('box6');
 
+const boxElements = [box1, box2, box3, box4, box5, box6]
 const arr = ['Gagik Galstyan','Gevorg Mirzoyan','Hovo Ohanyan','Artur Petrosyan','Mary Grigoryan']
 
 
@@ -18,18 +25,23 @@ forPaint()
 
 function shuffle () {
     const randomIndex = Math.floor(arr.length * Math.random())
+    const selectedPerson = arr[randomIndex]
     arr.splice(randomIndex, 1)
+
+    const randomBoxIndex = Math.floor(boxElements.length * Math.random())
+    const selectedBox = boxElements[randomBoxIndex]
+
+    const boxElement = document.createElement('div')
+    boxElement.classList.add('circle')
+    boxElement.innerText = selectedPerson
+    selectedBox.appendChild(boxElement)
+
     gridBox.innerHTML = ''
     wheelBlock.style.display = 'block'
     setTimeout (function () {
         wheelBlock.style.display = 'none'
     },2500)
     forPaint()
-    // giveTheResult()
 }
-
-// function giveTheResult () {
-    
-// }
 
 btn.addEventListener('click', shuffle)
